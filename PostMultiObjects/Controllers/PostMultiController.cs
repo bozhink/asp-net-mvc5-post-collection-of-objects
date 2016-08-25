22870c333d7d6e78a730a9125084874599cc3ac8
bozhink <bozhink@gmail.com>
2016-08-25 13:37:47 +0300

    UpdateInts is added.

---
+++ PostMultiObjects/Controllers/PostMultiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PostMultiObjects.Controllers
+{
+    public class PostMultiController : Controller
+    {
+        // GET: PostMulti
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public ActionResult UpdateInts()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateIntsPost(ICollection<int> ints)
+        {
+            return this.View(ints);
+        }
+    }
+}
